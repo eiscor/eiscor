a4509e3a8dd735610cfccebb754d83a8e736e744
Thomas Mach <thomas.mach@cs.kuleuven.be>
2015-01-08 15:33:27 +0100

    add a complex example to test_z_2x2array_eig

--- tests/complex_double/test_z_2x2array_eig.f90
+++ tests/complex_double/test_z_2x2array_eig.f90
@@ -8,11 +8,11 @@
 ! This program tests the subroutine test_z_2x2array_eig. 
 ! The following tests are run:
 !
-! 1) H = [1,2;2,1] +i0d0 (example from test_d_2x2array_eig)
+! 1) H = [1, 2; 2, 1] +i0d0 (example from test_d_2x2array_eig)
 !
 ! 2) H = [0.5, 0.3; 0.2, 0.3] +i0d0 (example from test_d_2x2array_eig)
 !
-! 3) H = 
+! 3) H = [1+i, 1; i, 3]
 !
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 program test_z_2x2array_eig
@@ -139,6 +139,44 @@ program test_z_2x2array_eig
      if (abs(Z(2,2)-cmplx(1d0/sqrt(1d1),sqrt(2d0)/2d0,kind=8))>tol) then
         call u_test_failed(__LINE__)
      end if     
+  end if
+
+
+  !check 3) 
+  H(1,1) = cmplx(1d0,1d0,kind=8)
+  H(1,2) = cmplx(2d0,0d0,kind=8)
+  H(2,1) = cmplx(0d0,1d0,kind=8)
+  H(2,2) = cmplx(3d0,0d0,kind=8)
+
+   call z_2x2array_eig(H,E,Z,INFO)
+  ! check info
+  if (INFO.NE.0) then
+     call u_test_failed(__LINE__)
+  end if
+ 
+  ! check results
+  ! check E
+  if ((abs(E(1)-cmplx(1d0,0.0d0,kind=8))>tol)&
+       &.AND.(abs(E(2)-cmplx(1d0,0.0d0,kind=8))>tol)) then
+    call u_test_failed(__LINE__)
+  end if
+  if ((abs(E(1)-cmplx(3d0,1d0,kind=8))>tol)&
+       &.AND.(abs(E(2)-cmplx(3d0,1d0,kind=8))>tol)) then
+    call u_test_failed(__LINE__)
+  end if
+
+  !check Z
+  if (abs(abs(Z(1,1))-cmplx(sqrt(2d0)/2d0,0d0,kind=8))>tol) then
+     call u_test_failed(__LINE__)
+  end if
+  if (abs(abs(Z(1,2))-cmplx(sqrt(2d0)/2d0,0d0,kind=8))>tol) then
+     call u_test_failed(__LINE__)
+  end if
+  if (abs(abs(Z(2,1))-cmplx(sqrt(2d0)/2d0,0d0,kind=8))>tol) then
+     call u_test_failed(__LINE__)
+  end if
+  if (abs(abs(Z(2,2))-cmplx(sqrt(2d0)/2d0,0d0,kind=8))>tol) then
+     call u_test_failed(__LINE__)
   end if
 
   ! stop timer
