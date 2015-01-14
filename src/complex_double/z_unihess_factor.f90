1a50fd0be32e2b7fdaef9ebc8b2743f0cf9badc6
Thomas Mach <thomas.mach@cs.kuleuven.be>
2015-01-14 16:02:41 +0100

    add missing call

--- src/complex_double/z_unihess_factor.f90
+++ src/complex_double/z_unihess_factor.f90
@@ -110,12 +110,12 @@ subroutine z_unihess_factor(N,H,Q,D,INFO)
     Q(3*(ii-1)+3) = s
           
     ! store in D
-    d_rot2_vec2gen(dble(H(ii,ii)),aimag(H(ii,ii)),D(2*(ii-1)+1),D(2*(ii-1)+2),nrm)
+    call d_rot2_vec2gen(dble(H(ii,ii)),aimag(H(ii,ii)),D(2*(ii-1)+1),D(2*(ii-1)+2),nrm)
    
   end do
   
   ! store in D
-  d_rot2_vec2gen(dble(H(N,N)),aimag(H(N,N)),D(2*(N-1)+1),D(2*(N-1)+2),nrm)
+  call d_rot2_vec2gen(dble(H(N,N)),aimag(H(N,N)),D(2*(N-1)+1),D(2*(N-1)+2),nrm)
   
   ! check for unitarity       
   if (abs(nrm-1d0) >= tol) then
