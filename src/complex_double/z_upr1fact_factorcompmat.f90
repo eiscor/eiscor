217352d81e6ab12feeb96f9c2306a6fb85fbcd8d
Jared Aurentz <aurentz@maths.ox.ac.uk>
2015-01-19 07:30:33 +0000

    finished QR portion of z_upr1fact_factorcompmat

--- src/complex_double/z_upr1fact_factorcompmat.f90
+++ src/complex_double/z_upr1fact_factorcompmat.f90
@@ -1,3 +1,4 @@
+#include "eiscor.h"
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !
 ! z_upr1fact_factorcompmat
@@ -14,7 +15,7 @@
 !  N               INTEGER
 !                    dimension of matrix
 !
-!  COEFFS          COMPLEX(8) array of dimension (N)
+!  A1          COMPLEX(8) array of dimension (N)
 !                   coefficients of polynomial, assumed to be of degree
 !                   exactly N, have leading coefficient 1 and have no 
 !                   zero roots
@@ -23,15 +24,11 @@
 !                    array of generators for first sequence of Givens' 
 !                    rotations
 !
-!  D               REAL(8) array of dimension (2*(N+1))
-!                    array of generators for complex diagonal matrix
+!  D1, D2          REAL(8) array of dimension (2*(N+1))
+!                    arrays of generators for complex diagonal matrices
 !
-!  C               REAL(8) array of dimension (3*N)
-!                    array of generators for second sequence of Givens' 
-!                    rotations
-!
-!  B               REAL(8) array of dimension (3*N)
-!                    array of generators for third sequence of Givens' 
+!  C1, B1, C2, B2  REAL(8) array of dimension (3*N)
+!                    arrays of generators for second sequence of Givens' 
 !                    rotations
 !
 ! OUTPUT VARIABLES:
@@ -42,148 +39,133 @@
 !                   an improper value, i.e. INFO=-2 => N is invalid
 !
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-subroutine z_upr1fact_factorcompmat(COMPZ,N,COEFFS,Q,D,C,B,Z,INFO)
+subroutine z_upr1fact_factorcompmat(ALG,COMPZ,N,A1,A2,Q,D1,C1,B1,D2,C2,B2,V,W,INFO)
 
   implicit none
   
   ! input variables
+  character(2), intent(in) :: ALG
   character, intent(in) :: COMPZ
   integer, intent(in) :: N
-  complex(8), intent(in) :: COEFFS(N)
-  real(8), intent(inout) :: Q(3*N), D(2*(N+1)), C(3*N), B(3*N)
-  complex(8), intent(inout) :: Z(N,N)
+  complex(8), intent(in) :: A1(N), A2(N)
+  real(8), intent(inout) :: Q(3*N), D1(2*(N+1)), D2(2*(N+1))
+  real(8), intent(inout) :: C1(3*N), B1(3*N), C2(3*N), B2(3*N)
+  complex(8), intent(inout) :: V(N,N), W(N,N)
   integer, intent(inout) :: INFO
   
   ! compute variables
   integer :: ii, ind
-  real(8) :: nrm
-  complex(8) :: t1, t2, phase
+  real(8) :: phr, phi, nrm
+  complex(8) :: t1, t2
   
   ! initialize INFO
   INFO = 0
   
-  ! check COMPZ
-  if ((COMPZ.NE.'N').AND.(COMPZ.NE.'I').AND.(COMPZ.NE.'V')) then
-    INFO = -1
-    write(*,*) "Error in "//__FILE__//" line:",__LINE__
-    write(*,*) "COMPZ must be 'N', 'I' or 'V'"
-    write(*,*) ""
-    return
-  end if
-  
-  ! check N
-  call IARNAN(N,INFO)
-  if (INFO.NE.0) then
-    INFO = -2
-    write(*,*) "Error in "//__FILE__//" line:",__LINE__
-    write(*,*) "N contains a NAN."
-    write(*,*) ""
-    return
-  end if
-  call IARINF(N,INFO)
-  if (INFO.NE.0) then
-    INFO = -2
-    write(*,*) "Error in "//__FILE__//" line:",__LINE__
-    write(*,*) "N contains an INF."
-    write(*,*) ""
-    return
-  end if
-  if (N < 2) then
-    INFO = -2
-    write(*,*) "Error in "//__FILE__//" line:",__LINE__
-    write(*,*) "N must be at least 2."
-    write(*,*) ""
-    return
-  end if
-  
-  ! check COEFFS
-  call ZARACH1(N,COEFFS,INFO)
-  if (INFO.NE.0) then
-    INFO = -3
-    write(*,*) "Error in "//__FILE__//" line:",__LINE__
-    write(*,*) "COEFFS contains an INF or NAN"
-    write(*,*) ""
-    return
-  end if
-  if (abs(COEFFS(N)).EQ.0d0) then
-    INFO = -3
-    write(*,*) "Error in "//__FILE__//" line:",__LINE__
-    write(*,*) "COEFFS has a zero root."
-    write(*,*) ""
-    return
-  end if
+  ! check input
+  if (DEBUG) then
 
-  ! check Z
-  if (COMPZ.EQ.'V') then
-    call ZARACH2(N,N,Z,INFO)
-    if (INFO.NE.0) then
-      INFO = -8
-      write(*,*) "Error in "//__FILE__//" line:",__LINE__
-      write(*,*) "Z contains an INF or NAN"
-      write(*,*) ""
-      return
-    end if
   end if      
   
-  ! compute the phase of COEFFS(N)
-  phase = COEFFS(N)
-  nrm = abs(phase)
-  phase = phase/nrm
-
-  ! initialize Z
+  ! initialize V
   if (COMPZ.EQ.'I') then
-    Z = cmplx(0d0,0d0,kind=8)
+    V = cmplx(0d0,0d0,kind=8)
     do ii=1,N
-      Z(ii,ii) = cmplx(1d0,0d0,kind=8)
+      V(ii,ii) = cmplx(1d0,0d0,kind=8)
     end do
   end if
   
-  ! update Z
-  if (COMPZ.NE.'N') then
-    Z(:,N) = conjg(phase)*Z(:,N)
-  end if
+  ! QR factorization
+  if (ALG.EQ.'QR') then
   
-  ! set Q
-  do ii=1,(N-1)
-    ind = 3*(ii-1)
-    Q(ind+1) = 0d0
+    ! compute the phase of A1(N)
+    call d_rot2_vec2gen(dble(A1(N)),aimag(A1(N)),phr,phi,nrm,INFO)
+    
+    ! check INFO in debug mode
+    if (DEBUG) then
+      call u_infocode_check(__FILE__,__LINE__,"d_rot2_vec2gen",INFO,INFO)
+      if (INFO.NE.0) then 
+        return 
+      end if 
+    end if
+    
+    ! update V
+    if (COMPZ.NE.'N') then
+      V(:,N) = cmplx(phr,-phi,kind=8)*V(:,N)
+    end if
+    
+    ! set Q
+    do ii=1,(N-1)
+      ind = 3*(ii-1)
+      Q(ind+1) = 0d0
+      Q(ind+2) = 0d0
+      Q(ind+3) = 1d0
+    end do
+    ind = 3*(N-1)
+    Q(ind+1) = 1d0
     Q(ind+2) = 0d0
-    Q(ind+3) = 1d0
-  end do
-  ind = 3*(N-1)
-  Q(ind+1) = 1d0
-  Q(ind+2) = 0d0
-  Q(ind+3) = 0d0
+    Q(ind+3) = 0d0
+    
+    ! set D1
+    do ii=1,N+1
+      ind = 2*(ii-1)
+      D1(ind+1) = 1d0
+      D1(ind+2) = 0d0
+    end do
+    ind = 2*(N-2)
+    D1(ind+1) = phr
+    D1(ind+2) = phi
+    
+    ! initialize B1 and C1
+    t1 = cmplx(nrm*(-1d0)**(N),0d0,kind=8)
+    t2 = cmplx((-1d0)**(N-1),0d0,kind=8)
+    
+    ind = 3*(N-1)
+    call d_rot2_vec2gen(dble(t1),dble(t2),C1(ind+1),C1(ind+3),nrm,INFO)
+    
+    ! check INFO in debug mode
+    if (DEBUG) then
+      call u_infocode_check(__FILE__,__LINE__,"d_rot2_vec2gen",INFO,INFO)
+      if (INFO.NE.0) then 
+        return 
+      end if 
+    end if
+    
+    C1(ind+2) = 0d0
+    B1(ind+1) = C1(ind+3)*(-1d0)**(N)
+    B1(ind+2) = 0d0
+    B1(ind+3) = C1(ind+1)*(-1d0)**(N)
+    
+    do ii=2,N
+      ind = 3*(N-ii+1)
+      t2 = cmplx(C1(ind+1),-C1(ind+2),kind=8)*t1 + cmplx(C1(ind+3),0d0,kind=8)*t2
+      t1 = -A1(ii-1)*cmplx(phr,-phi,kind=8)
+      ind = 3*(N-ii)
+      call z_rot3_vec4gen(dble(t1),aimag(t1),dble(t2),aimag(t2),C1(ind+1),C1(ind+2),C1(ind+3),nrm,INFO)
+      
+      ! check INFO in debug mode
+      if (DEBUG) then
+        call u_infocode_check(__FILE__,__LINE__,"z_rot3_vec4gen",INFO,INFO)
+        if (INFO.NE.0) then 
+          return 
+        end if 
+      end if      
+      
+      B1(ind+1) = C1(ind+1)
+      B1(ind+2) = -C1(ind+2)
+      B1(ind+3) = -C1(ind+3)
+    end do
   
-  ! set D
-  do ii=1,N+1
-    ind = 2*(ii-1)
-    D(ind+1) = 1d0
-    D(ind+2) = 0d0
-  end do
-  ind = 2*(N-2)
-  D(ind+1) = dble(phase)
-  D(ind+2) = aimag(phase)
+  ! QZ factorization
+  else
   
-  ! initialize B and C
-  t1 = cmplx(nrm*(-1d0)**(N),0d0,kind=8)
-  t2 = cmplx((-1d0)**(N-1),0d0,kind=8)
-  ind = 3*(N-1)
-  call DARCG22(nrm*(-1d0)**(N),(-1d0)**(N-1),C(ind+1),C(ind+3),nrm,INFO)
-  C(ind+2) = 0d0
-  B(ind+1) = C(ind+3)*(-1d0)**(N)
-  B(ind+2) = 0d0
-  B(ind+3) = C(ind+1)*(-1d0)**(N)
+    ! initialize W
+    if (COMPZ.EQ.'I') then
+      W = cmplx(0d0,0d0,kind=8)
+      do ii=1,N
+        W(ii,ii) = cmplx(1d0,0d0,kind=8)
+      end do
+    end if  
   
-  do ii=2,N
-    ind = 3*(N-ii+1)
-    t2 = cmplx(C(ind+1),-C(ind+2),kind=8)*t1 + cmplx(C(ind+3),0d0,kind=8)*t2
-    t1 = -COEFFS(ii-1)*conjg(phase)
-    ind = 3*(N-ii)
-    call ZARCG43(dble(t1),aimag(t1),dble(t2),aimag(t2),C(ind+1),C(ind+2),C(ind+3),nrm,INFO)
-    B(ind+1) = C(ind+1)
-    B(ind+2) = -C(ind+2)
-    B(ind+3) = -C(ind+3)
-  end do
+  end if
 
 end subroutine z_upr1fact_factorcompmat
