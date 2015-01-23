d7cb0861a2dd356d8bc08b32a34162d5d344bfa4
Jared Aurentz <aurentz@maths.ox.ac.uk>
2015-01-23 14:37:13 +0000

    added d_rot2_check and associated test

--- src/double/d_rot2_check.f90
+++ src/double/d_rot2_check.f90
@@ -1,27 +1,26 @@
 #include "eiscor.h"
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !
-! z_rot3_check
+! d_rot2_check
 !
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !
-! This routine checks the generators for a complex rotation represented
-! by 3 real numbers: the real and imaginary parts of a complex cosine,
-! CR and CI, and a scrictly real sine, S. 
+! This routine checks the generators for a real rotation represented
+! by 2 real numbers: a real cosine, C, and a real sine, S. 
 !
-! To check that it is valid, a new rotation XR, XI, Y is computed. If
-! |CR-XR|, |CI-XI|, |S-Y|, |NRM-1| < 3*EISCOR_DBL_EPS the generators are 
+! To check that it is valid, a new rotation X, Y is computed. If
+! |C-X|, |S-Y|, |NRM-1| < 2*EISCOR_DBL_EPS the generators are 
 ! considered valid. Otherwise they are invalid.
 !
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !
 ! INPUT VARIABLES:
 !
-!  CR, CI          REAL(8)
-!                    generators for complex cosine CR + iCI
+!  C               REAL(8)
+!                    generators for cosine
 !
 !  S               REAL(8)
-!                    generator for real sine S
+!                    generator for sine
 !
 ! OUTPUT VARIABLES:
 !
@@ -30,27 +29,27 @@
 !                    .FALSE. implies invalid generators
 !
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-subroutine z_rot3_check(CR,CI,S,FLAG)
+subroutine d_rot2_check(C,S,FLAG)
 
   implicit none
   
   ! input variables
-  real(8), intent(in) :: CR, CI, S
+  real(8), intent(in) :: C, S
   logical, intent(inout) :: FLAG
   
   ! compute variables
-  real(8), parameter :: tol = 3d0*EISCOR_DBL_EPS
-  real(8) :: XR, XI, Y, NRM
+  real(8), parameter :: tol = 2d0*EISCOR_DBL_EPS
+  real(8) :: X, Y, NRM
 
   ! initialize FLAG to .FALSE.
   FLAG = .FALSE.
   
   ! compute new rotation from input one
-  call z_rot3_vec3gen(CR,CI,S,XR,XI,Y,NRM)
+  call d_rot2_vec2gen(C,S,X,Y,NRM)
   
   ! check for equality
-  if ((abs(CR-XR)<tol).AND.(abs(CI-XI)<tol).AND.(abs(S-Y)<tol).AND.(abs(1d0-NRM)<tol)) then
+  if ((abs(C-X)<tol).AND.(abs(S-Y)<tol).AND.(abs(1d0-NRM)<tol)) then
     FLAG = .TRUE.
   end if
 
-end subroutine z_rot3_check
+end subroutine d_rot2_check
