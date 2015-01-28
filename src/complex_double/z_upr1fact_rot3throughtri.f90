c8cac85778bf51b1d8a0198de93b6840355eccde
Jared Aurentz <aurentz@maths.ox.ac.uk>
2015-01-28 17:34:46 +0000

    new flag for rot3throughtri for identity case

--- src/complex_double/z_upr1fact_rot3throughtri.f90
+++ src/complex_double/z_upr1fact_rot3throughtri.f90
@@ -13,9 +13,13 @@
 !
 ! INPUT VARIABLES:
 !
-!  FLAG            LOGICAL
+!  DIR             LOGICAL
 !                    .TRUE.: pass rotation from left to right
 !                    .FALSE.: pass rotation from right to left
+!
+!  FLAG            LOGICAL
+!                    .TRUE.: pass rotation through C and B
+!                    .FALSE.: ignore C and B
 !
 !  D               REAL(8) array of dimension (4)
 !                    array of generators for complex diagonal matrices
@@ -31,37 +35,47 @@
 !                    generator for rotation
 !
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-subroutine z_upr1fact_rot3throughtri(FLAG,D,C,B,G)
+subroutine z_upr1fact_rot3throughtri(DIR,FLAG,D,C,B,G)
 
   implicit none
   
   ! input variables
-  logical, intent(in) :: FLAG
+  logical, intent(in) :: DIR, FLAG
   real(8), intent(inout) :: D(4), C(6), B(6), G(3)
   
   ! L2R
-  if (FLAG) then
+  if (DIR) then
   
     ! through D
-    call z_rot3_swapdiag(FLAG,D,G)
+    call z_rot3_swapdiag(DIR,D,G)
     
-    ! though C
-    call z_rot3_turnover(C(1:3),C(4:6),G)
+    ! through C and B
+    if (FLAG) then
     
-    ! through B
-    call z_rot3_turnover(B(4:6),B(1:3),G)
+      ! though C
+      call z_rot3_turnover(C(1:3),C(4:6),G)
+      
+      ! through B
+      call z_rot3_turnover(B(4:6),B(1:3),G)
+    
+    end if
   
   ! R2L
   else
   
-    ! through B
-    call z_rot3_turnover(B(1:3),B(4:6),G)
-    
-    ! through C
-    call z_rot3_turnover(C(4:6),C(1:3),G)
+    ! through C and B
+    if (FLAG) then
+  
+      ! through B
+      call z_rot3_turnover(B(1:3),B(4:6),G)
+      
+      ! through C
+      call z_rot3_turnover(C(4:6),C(1:3),G)
+      
+    end if
     
     ! through D
-    call z_rot3_swapdiag(FLAG,D,G)
+    call z_rot3_swapdiag(DIR,D,G)
   
   end if
 
