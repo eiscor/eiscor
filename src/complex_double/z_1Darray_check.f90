afcbf2bce5010d22cdd670469627c63ce17ae9e6
Jared Aurentz <aurentz@maths.ox.ac.uk>
2015-01-29 09:07:05 +0000

    added z_1Darray_check and associated test

--- src/complex_double/z_1Darray_check.f90
+++ src/complex_double/z_1Darray_check.f90
@@ -1,39 +1,37 @@
 #include "eiscor.h"
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !
-! z_rot3array_check
+! z_1Darray_check 
 !
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !
-! This routine checks the validity of an array of rot3 generators.
+! This routine checks a one dimensional complex array for INFs and NANs.
 !
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !
 ! INPUT VARIABLES:
 !
 !  N               INTEGER
-!                    positive integer, number of rot3 generators in A
+!                    positive integer, dimension of A
 !
-!  A               REAL(8) array of dimension (3*N)
-!                    real array of rot3 generators to be checked
-!                    generators are stored sequentially: 
-!                                  A(1:6) = {CR1,CI1,S1,CR2,CI2,S2}
+!  A               COMPLEX(8) array of dimension (N)
+!                    complex array to be checked
 !
 ! OUTPUT VARIABLES:
 !
 !  FLAG            LOGICAL
-!                    .TRUE. implies A contains valid rot3 generators
+!                    .TRUE. implies A contains valid numbers
 !                    .FALSE. implies N < 1 or A contains at least one invalid 
-!                            rot3 generator
+!                            number
 !
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-subroutine z_rot3array_check(N,A,FLAG)
+subroutine z_1Darray_check(N,A,FLAG)
 
   implicit none
   
   ! input variables
   integer, intent(in) :: N
-  real(8), intent(in) :: A(3*N)
+  complex(8), intent(in) :: A(N)
   logical, intent(inout) :: FLAG
   
   ! compute variables
@@ -47,15 +45,15 @@ subroutine z_rot3array_check(N,A,FLAG)
     return
   end if
   
-  ! check A
+  ! check array
   do ii = 1,N
   
-    ! check single generator
-    call z_rot3_check(A(3*ii-2),A(3*ii-1),A(3*ii),FLAG)
+    ! check for INF or NAN
+    call z_scalar_check(A(ii),FLAG)
     if (.NOT.FLAG) then
       return
     end if
     
   end do
 
-end subroutine z_rot3array_check
+end subroutine z_1Darray_check
