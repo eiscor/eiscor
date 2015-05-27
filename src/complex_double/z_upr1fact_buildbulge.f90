dab5d7bc4f1ac5bbbb532081785ad0d0c053503d
Jared <aurentz@maths.ox.ac.uk>
2015-05-27 14:43:08 +0200

    updated z_upr1fact_buildbulge for 4 parameter Q

--- src/complex_double/z_upr1fact_buildbulge.f90
+++ src/complex_double/z_upr1fact_buildbulge.f90
@@ -5,7 +5,7 @@
 !
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !
-! This routine computes the first transformation for a sinlge shift
+! This routine computes the first transformation for a single shift
 ! iteration on a upr1 pencil.
 !
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -19,13 +19,8 @@
 !  P               LOGICAL 
 !                    position flag for Q
 !
-!  Q               REAL(8) array of dimension (6)
+!  Q               REAL(8) array of dimension (4)
 !                    array of generators for first sequence of rotations
-!
-!  D1,D2           REAL(8) arrays of dimension (4)
-!                    array of generators for complex diagonal matrices
-!                    in the upper-triangular factors
-!                    If QZ == .FALSE., D2 is unused.
 !
 !  C1,B1,C2,B2     REAL(8) arrays of dimension (6)
 !                    array of generators for upper-triangular parts
@@ -42,14 +37,13 @@
 !                    transformation
 !
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-subroutine z_upr1fact_buildbulge(QZ,P,Q,D1,C1,B1,D2,C2,B2,SHFT,G)
+subroutine z_upr1fact_buildbulge(QZ,P,Q,C1,B1,C2,B2,SHFT,G)
   
   implicit none
   
   ! input variables
   logical, intent(in) :: QZ, P
-  real(8), intent(in) :: Q(6), D1(4), C1(6), B1(6)
-  real(8), intent(in) :: D2(4), C2(6), B2(6)
+  real(8), intent(in) :: Q(4), C1(6), B1(6), C2(6), B2(6)
   complex(8), intent(in) :: SHFT
   real(8), intent(inout) :: G(3)
   
@@ -57,11 +51,13 @@ subroutine z_upr1fact_buildbulge(QZ,P,Q,D1,C1,B1,D2,C2,B2,SHFT,G)
   real(8) :: nrm
   complex(8) :: A(2,2), B(2,2), vec1(2), vec2(2)
   
-  ! get 2x2 blocks
-  call z_upr1fact_2x2diagblocks(.TRUE.,.FALSE.,QZ,P,Q,D1,C1,B1,D2,C2,B2,A,B)
-     
-  ! set B to I of not QZ
-  if (.NOT.QZ) then
+  ! get first triangle
+  call z_upr1fact_extracttri(.FALSE.,2,C1,B1,A)
+
+  ! get second triangle
+  if (QZ) then
+    call z_upr1fact_extracttri(.FALSE.,2,C2,B2,B)
+  else 
     B = cmplx(0d0,0d0,kind=8)
     B(1,1) = cmplx(1d0,0d0,kind=8); B(2,2) = B(1,1)
   end if
@@ -72,7 +68,7 @@ subroutine z_upr1fact_buildbulge(QZ,P,Q,D1,C1,B1,D2,C2,B2,SHFT,G)
    
     ! first column of A
     vec1(1) = cmplx(Q(1),Q(2),kind=8)*A(1,1)
-    vec1(2) = cmplx(Q(3),0d0,kind=8)*A(1,1)
+    vec1(2) = cmplx(Q(3),Q(4),kind=8)*A(1,1)
     
     ! first column of B
     vec2(1) = B(1,1)
@@ -81,9 +77,9 @@ subroutine z_upr1fact_buildbulge(QZ,P,Q,D1,C1,B1,D2,C2,B2,SHFT,G)
   ! P == TRUE
   else
     
-    ! Q*e1
+    ! Q^H e1
     vec2(1) = cmplx(Q(1),-Q(2),kind=8)
-    vec2(2) = cmplx(-Q(3),0d0,kind=8)
+    vec2(2) = cmplx(-Q(3),Q(4),kind=8)
     
     ! back solve with A
     vec2(2) = vec2(2)/A(2,2)
