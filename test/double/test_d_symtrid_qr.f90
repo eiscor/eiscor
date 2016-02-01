150530f5a4d7eb4085f7d4548435a9a5920465b0
Thomas Mach <thomas.mach@cs.kuleuven.be>
2016-02-01 11:31:31 +0100

    adding tests to test_d_symtrid_qr.f90 to check whether the d_symtrid_qr can handle deflateable tridiagonal matrices, it can

--- test/double/test_d_symtrid_qr.f90
+++ test/double/test_d_symtrid_qr.f90
@@ -6,8 +6,14 @@
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !
 ! This routine computes the eigenvalues of the tridiagonal matrix
-! [ -1 2 -1 ] testing the forward error and of a random normally 
+! [-0.5 0 -0.5] testing the forward error and of a random normally 
 ! distributed matrix testing the backward error.  
+!
+! check 1) [-0.5 0 -0.5]
+! check 2) random tridiagonal matrix
+! check 3) random tridiagonal matrix with first row/column zero
+! check 4) random tridiagonal matrix with last row/column zero
+! check 5) random tridiagonal matrix with row 5 and column 5 zero
 !
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 program test_d_symtrid_qr  
@@ -40,6 +46,7 @@ program test_d_symtrid_qr
   ! print banner
   call u_test_banner(__FILE__)
 
+  ! check 1) [-0.5 0 -0.5]
   ! initialize T to be a tridiagonal matrix of the form
   !  2 -1
   ! -1  2 -1
@@ -132,7 +139,9 @@ program test_d_symtrid_qr
      ! backward error test failed
      call u_test_failed(__LINE__)
   end if
-  
+
+
+  ! check 2) random tridiagonal matrix
   !call u_randomseed_initialize(INFO)
   call u_fixedseed_initialize(INFO)
   do ii=1,N-1
@@ -180,6 +189,157 @@ program test_d_symtrid_qr
      call u_test_failed(__LINE__)
   end if
   
+  ! check 3) random tridiagonal matrix with first row/column zero
+  call u_fixedseed_initialize(INFO)
+  do ii=1,N-1
+     call random_number(t)
+     Ds(ii) = t
+     call random_number(t)
+     Es(ii) = t
+  end do
+  call random_number(t)
+  Ds(N) = t
+
+  Ds(1) = 0d0
+  Es(1) = 0d0
+  
+  D = Ds
+  E = Es
+
+  call d_symtrid_qr(.TRUE.,.TRUE.,sca,N,D,E,WORK,N,Z,ITS,INFO)
+
+  ! computing backward error
+  t2 = 0d0
+  do ii=1,N
+     ! jj  1
+     v(1) = (Ds(1)-D(ii))*Z(1+N*(ii-1),1) + Es(1)*Z(2+N*(ii-1),1)
+     do jj=2,N-1
+        v(jj) = (Ds(jj)-D(ii))*Z(jj+N*(ii-1),1) + &
+             & Es(jj)*Z(jj+1+N*(ii-1),1) + &
+             & Es(jj-1)*Z(jj-1+N*(ii-1),1)
+     end do
+     v(N) = (Ds(N)-D(ii))*Z(N+N*(ii-1),1) + &
+          & Es(N-1)*Z(N-1+N*(ii-1),1)
+
+     t3 = 0d0
+     do jj=1,N
+        t3 = t3 + abs(v(jj))**2
+     end do
+     t3 = sqrt(t3)
+
+     if (t3.GT.t2) then
+        t2 = t3
+     end if
+  end do
+
+  t2 = t2/nrm
+
+  if ((t2>1d-14).OR.(t2.NE.t2)) then
+     ! backward error test failed
+     call u_test_failed(__LINE__)
+  end if
+
+  ! check 4) random tridiagonal matrix with last row/column zero
+  call u_fixedseed_initialize(INFO)
+  do ii=1,N-1
+     call random_number(t)
+     Ds(ii) = t
+     call random_number(t)
+     Es(ii) = t
+  end do
+  call random_number(t)
+  Ds(N) = t
+
+  Ds(N) = 0d0
+  Es(N-1) = 0d0
+  
+  D = Ds
+  E = Es
+
+  call d_symtrid_qr(.TRUE.,.TRUE.,sca,N,D,E,WORK,N,Z,ITS,INFO)
+
+  ! computing backward error
+  t2 = 0d0
+  do ii=1,N
+     ! jj  1
+     v(1) = (Ds(1)-D(ii))*Z(1+N*(ii-1),1) + Es(1)*Z(2+N*(ii-1),1)
+     do jj=2,N-1
+        v(jj) = (Ds(jj)-D(ii))*Z(jj+N*(ii-1),1) + &
+             & Es(jj)*Z(jj+1+N*(ii-1),1) + &
+             & Es(jj-1)*Z(jj-1+N*(ii-1),1)
+     end do
+     v(N) = (Ds(N)-D(ii))*Z(N+N*(ii-1),1) + &
+          & Es(N-1)*Z(N-1+N*(ii-1),1)
+
+     t3 = 0d0
+     do jj=1,N
+        t3 = t3 + abs(v(jj))**2
+     end do
+     t3 = sqrt(t3)
+
+     if (t3.GT.t2) then
+        t2 = t3
+     end if
+  end do
+
+  t2 = t2/nrm
+
+  if ((t2>1d-14).OR.(t2.NE.t2)) then
+     ! backward error test failed
+     call u_test_failed(__LINE__)
+  end if
+
+  ! check 5) random tridiagonal matrix with row 5 and column 5 zero
+  call u_fixedseed_initialize(INFO)
+  do ii=1,N-1
+     call random_number(t)
+     Ds(ii) = t
+     call random_number(t)
+     Es(ii) = t
+  end do
+  call random_number(t)
+  Ds(N) = t
+
+  Ds(5) = 0d0
+  Es(4) = 0d0
+  Es(5) = 0d0
+  
+  D = Ds
+  E = Es
+
+  call d_symtrid_qr(.TRUE.,.TRUE.,sca,N,D,E,WORK,N,Z,ITS,INFO)
+
+  ! computing backward error
+  t2 = 0d0
+  do ii=1,N
+     ! jj  1
+     v(1) = (Ds(1)-D(ii))*Z(1+N*(ii-1),1) + Es(1)*Z(2+N*(ii-1),1)
+     do jj=2,N-1
+        v(jj) = (Ds(jj)-D(ii))*Z(jj+N*(ii-1),1) + &
+             & Es(jj)*Z(jj+1+N*(ii-1),1) + &
+             & Es(jj-1)*Z(jj-1+N*(ii-1),1)
+     end do
+     v(N) = (Ds(N)-D(ii))*Z(N+N*(ii-1),1) + &
+          & Es(N-1)*Z(N-1+N*(ii-1),1)
+
+     t3 = 0d0
+     do jj=1,N
+        t3 = t3 + abs(v(jj))**2
+     end do
+     t3 = sqrt(t3)
+
+     if (t3.GT.t2) then
+        t2 = t3
+     end if
+  end do
+
+  t2 = t2/nrm
+
+  if ((t2>1d-14).OR.(t2.NE.t2)) then
+     ! backward error test failed
+     call u_test_failed(__LINE__)
+  end if
+
   ! stop timer
   call system_clock(count=c_stop)
   
