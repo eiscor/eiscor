2df86e9bf7ad69aa915bca7cae13c75e2ad2b920
Thomas Mach <thomas.mach@googlemail.com>
2016-10-20 13:10:26 +0600

    copying in the fast turnover from the feature-fast-turnover branch

--- src/complex_double/z_rot3_turnover.f90
+++ src/complex_double/z_rot3_turnover.f90
@@ -34,12 +34,10 @@ subroutine z_rot3_turnover(G1,G2,G3)
   real(8), intent(inout) :: G1(3), G2(3), G3(3)
 
   ! compute variables
-  real(8) :: nrm, T(3)
-  
+  real(8) :: nrm, ar, ai, b
   real(8) :: c1r, c1i, s1
   real(8) :: c2r, c2i, s2
   real(8) :: c3r, c3i, s3
-  
   real(8) :: c4r, c4i, s4
   real(8) :: c5r, c5i, s5
   real(8) :: c6r, c6i, s6
@@ -55,64 +53,25 @@ subroutine z_rot3_turnover(G1,G2,G3)
   c3i = G3(2)
   s3 = G3(3)
 
-  if ((s1.EQ.0d0) .AND. (s3.EQ.0d0)) then
-     ! the case s1=s3=0d0 is special
-     ! using the procedure for the generic case results in a rotation with
-     ! c=1 and s=0 and in a rotation with a not necessarily real sine
-     
-     ! initialize c4r, c4i and s4
-     T(1) = c1r*c2r + c1i*c2i
-     T(2) = -c1i*c2r + c1r*c2i
-     T(3) = s2
-     
-     ! compute first rotation
-     call z_rot3_vec3gen(T(1),T(2),T(3),c4r,c4i,s4,nrm)
-     
-     ! initialize c5r, c5i and s5
-     T(1) = c1r*c3r - c1i*c3i 
-     T(2) = c1r*c3i + c1i*c3r 
-     T(3) = 0d0
-     
-     ! compute second rotation
-     call z_rot3_vec3gen(T(1),T(2),T(3),c5r,c5i,s5,nrm)
-     
-     ! initialize c6r, c6i and s6
-     T(1) = c2r*c4r + c2i*c4i + c1r*s2*s4
-     T(2) = c2i*c4r - c2r*c4i + c1i*s2*s4
-     T(3) = s1*s2*s5 - (c5r*c2r + c5i*c2i)*s4 + s2*(c1r*(c4r*c5r + c4i*c5i) &
-     + c1i*(c4r*c5i - c4i*c5r))
-     
-     ! compute third rotation
-     call z_rot3_vec3gen(T(1),T(2),T(3),c6r,c6i,s6,nrm)
-     
-  else
-     ! initialize c4r, c4i and s4
-     T(1) = s1*c3r + (c1r*c2r + c1i*c2i)*s3
-     T(2) = s1*c3i + (-c1i*c2r + c1r*c2i)*s3
-     T(3) = s2*s3
-     
-     ! compute first rotation
-     call z_rot3_vec3gen(T(1),T(2),T(3),c4r,c4i,s4,nrm)
-     
-     ! initialize c5r, c5i and s5
-     T(1) = c1r*c3r - c1i*c3i - s1*c2r*s3
-     T(2) = c1r*c3i + c1i*c3r - s1*c2i*s3
-     T(3) = nrm
-     
-     ! compute second rotation
-     call z_rot3_vec3gen(T(1),T(2),T(3),c5r,c5i,s5,nrm)
-     
-     ! initialize c6r, c6i and s6
-     T(1) = c2r*c4r + c2i*c4i + c1r*s2*s4
-     T(2) = c2i*c4r - c2r*c4i + c1i*s2*s4
-     T(3) = s1*s2*s5 - (c5r*c2r + c5i*c2i)*s4 + s2*(c1r*(c4r*c5r + c4i*c5i) &
-     + c1i*(c4r*c5i - c4i*c5r))
-     
-     ! compute third rotation
-     call z_rot3_vec3gen(T(1),T(2),T(3),c6r,c6i,s6,nrm)
-
-  end if
-
+  ! compute first rotation
+  ar = s1*c3r + (c1r*c2r + c1i*c2i)*s3
+  ai = s1*c3i + (c1r*c2i - c1i*c2r)*s3
+  b = s2*s3
+  call z_rot3_vec3gen(ar,ai,b,c4r,c4i,s4,nrm)
+ 
+  ! compute second rotation
+  ar = c1r*c3r - c1i*c3i - s1*c2r*s3
+  ai = c1r*c3i + c1i*c3r - s1*c2i*s3
+  b = nrm
+  call z_rot3_vec3gen(ar,ai,b,c5r,c5i,s5,nrm)
+  
+  ! compute third rotation
+  ar = c2r*c4r + c2i*c4i + c1r*s2*s4
+  ai = c2i*c4r - c2r*c4i + c1i*s2*s4
+  b = (s1*s5 + c1i*(c5i*c4r - c4i*c5r) + c1r*(c4i*c5i + c4r*c5r))*s2 & 
+      - (c2i*c5i + c2r*c5r)*s4
+  call z_rot3_vec3gen(ar,ai,b,c6r,c6i,s6,nrm)
+  
   ! store output
   G1(1) = c5r
   G1(2) = c5i
