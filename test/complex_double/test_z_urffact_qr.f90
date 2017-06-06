ee8a219a34a398bf67f17d5d38a315b2d0ff26e6
Jared <jaredaurentz@gmail.com>
2017-06-06 09:30:47 +0200

    added correct normalization routines and updated test

--- test/complex_double/test_z_urffact_qr.f90
+++ test/complex_double/test_z_urffact_qr.f90
@@ -16,10 +16,13 @@ program test_z_urffact_qr
   
   ! compute variables
   integer, parameter :: N = 2**13
+  real(8), parameter :: tol = (EISCOR_DBL_EPS)
   integer :: ii, jj, INFO, ITCNT(N-1)
   complex(8) :: U(N)
-  real(8) :: VV(N)
-  real(8) :: tol
+  real(8) :: VV(N), A(N)
+  integer :: id
+  real(8) :: small, twopi
+  complex(8) :: swap, E(N)
   
   ! timing variables
   integer:: c_start, c_stop, c_rate
@@ -39,29 +42,56 @@ program test_z_urffact_qr
   U(N) = cmplx(sign(1d0,(-1d0)**(N-1)),0d0,kind=8)
   VV = 1d0
   VV(N) = 0d0
-!print*,""
-!print*,"U,VV"
-!do ii = 1,N
-!  print*,U(ii),VV(ii)
-!end do
     
   ! call singlestep
   call z_urffact_rfqr(N,U,VV,ITCNT,INFO)
-!print*,""
-!print*,"U,VV"
-!print*,U(1),VV(1)
-!do ii = 2,N
-!  print*,U(ii),VV(ii),ITCNT(ii-1)
-!end do
 
+  ! compute by argument
+  do ii = 1,N
+    call z_scalar_argument(dble(U(ii)),aimag(U(ii)),A(ii),INFO)
+  end do
 
-  ! set tolerance
-  tol = 10d0*EISCOR_DBL_EPS
+  ! sort by argument
+  do ii = 1,N
+    small = 10d0
+    id = ii
+    do jj = ii,N
+      if ( A(jj) < small ) then
+        id = jj
+        small = A(id)
+      end if
+    end do
+    A(id) = A(ii)
+    A(ii) = small
+    swap = U(id)
+    U(id) = U(ii)
+    U(ii) = swap
+  end do
     
-  ! check maximum entry
-!  if (maxval(abs(H(1:2,1:2))) >= tol) then
-!    call u_test_failed(__LINE__)
-!  end if  
+  ! true eigenvalues
+  if ( abs(U(1)-cmplx(1d0,0d0,kind=8)) < dble(N)*(EISCOR_DBL_EPS) ) then
+    id = 1
+  else
+    id = 0
+  end if
+  twopi = 2d0*(EISCOR_DBL_PI)
+  do ii = 1,N
+    small = twopi*dble(ii-id)/dble(N)
+    E(ii) = cmplx(cos(small),sin(small),kind=8)
+  end do
+
+  ! compute maximum forward error
+  small = 0d0
+  do ii = 1,N
+    if (abs(U(ii)-E(ii)) > small) then
+      small = abs(U(ii)-E(ii))
+    end if
+  end do
+
+  ! check maximum error
+  if (small >= dble(N)*tol) then
+    call u_test_failed(__LINE__)
+  end if  
 
   ! stop timer
   call system_clock(count=c_stop)
