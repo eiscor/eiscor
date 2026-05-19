ab0694d9ba4b259094042a9b3815a0f654335537
jared <jared.aurentz@dci.uhu.es>
2026-05-19 16:14:38 +0200

    updated usymfact singlestep

--- src/complex_double/z_usymfact_singlestep.f90
+++ src/complex_double/z_usymfact_singlestep.f90
@@ -1,130 +1,195 @@
 #include "eiscor.h"
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !
-! z_usymfact_singlestep 
+! z_usymfact_singlestep
 !
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !
-! This routine computes one iteration of Francis' singleshift algorithm on a
-! unitary upper hessenberg matrix that is stored as a product of givens
-! rotations. 
-!                                                                               
-! | nu  0 | | u1       -v1 |
-! |  0  1 | | v1  conj(u1) | | u2       -v2 | 
-!                            | v2  conj(u2) | | u3       -v3 | | 1   0 |
-!                                             | v3  conj(u3) | | 0  u4 | 
+! This routine computes one iteration of Francis' single-shift algorithm on a
+! unitary upper Hessenberg matrix stored as a product of Givens rotations.
 !
-! The square root free algorithm only requires the storage of the vi^2,
-! so the arrays U and V contain the following:
+! The matrix is represented by
 !
-!  U(i) = ui
-!  V(i) = vi
+!   | nu  0 | | u1       -v1 |
+!   |  0  1 | | v1  conj(u1) | | u2       -v2 |
+!                              | v2  conj(u2) | | u3       -v3 | | 1   0 |
+!                                               | v3  conj(u3) | | 0  u4 |
 !
-! The input must satisfy the following:
+! The arrays U and V contain
 !
-!  |U(i)|^2 + V(i)^2 = 1
-!               V(N) = 0
-!               |NU| = 1
+!     U(i) = ui,
+!     V(i) = vi.
+!
+! The input must satisfy
+!
+!     |U(i)|^2 + V(i)^2 = 1,   i = 1,...,N-1,
+!                  V(N) = 0,
+!                  |NU| = 1.
+!
+! If VEC = .TRUE., the eigenvector/similarity matrix Z is updated by the
+! same similarity transformations used in the QR step.
 !
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !
 ! INPUT VARIABLES:
 !
-!  N               INTEGER 
+!  VEC             LOGICAL
+!                    .TRUE.: update eigenvectors/similarity matrix
+!                    .FALSE.: do not update Z
+!
+!  N               INTEGER
 !                    dimension of matrix, must be >= 2
 !
 !  U               COMPLEX(8) array of dimension N
-!                    array of complex generators for Givens rotations
+!                    complex generators of the core transformations
 !
 !  V               REAL(8) array of dimension N
-!                    array of real generators for Givens rotations
+!                    real generators of the core transformations
 !
 !  NU              COMPLEX(8)
-!                    unimodular phase 
+!                    leading unimodular phase
+!
+!  M               INTEGER
+!                    leading dimension / number of rows of Z
+!
+!  Z               COMPLEX(8) array of dimension (M,N)
+!                    if VEC = .TRUE., updated by right multiplication
+!                    if VEC = .FALSE., unused
 !
 !  ITCNT           INTEGER
-!                    Contains the number of iterations since last deflation
+!                    iteration counter since last deflation
 !
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-subroutine z_usymfact_singlestep(N,U,V,NU,ITCNT)
+subroutine z_usymfact_singlestep(VEC,N,U,V,NU,M,Z,ITCNT)
 
   implicit none
-  
-  ! input variables
-  integer, intent(in) :: N
+
+  ! input/output variables
+  logical, intent(in) :: VEC
+  integer, intent(in) :: N, M
   integer, intent(inout) :: ITCNT
   complex(8), intent(inout) :: U(N)
   real(8), intent(inout) :: V(N)
   complex(8), intent(in) :: NU
-  
+  complex(8), intent(inout) :: Z(M,N)
+
   ! compute variables
-  integer :: ii
+  integer :: ii, jj
   real(8) :: vt, c, s, xx
-  complex(8) :: ut, w, rho
+  complex(8) :: ut, rho
+  complex(8) :: gamma, sigma
+  complex(8) :: z1, z2
   complex(8) :: block(2,2), t1(2,2), t2(2,2)
 
-  ! get 2x2 block
+  ! get trailing 2 x 2 block used for the projected Wilkinson shift
   if (N.EQ.2) then
     block(1,1) =  NU*U(N-1)
     block(2,2) =  conjg(NU*U(N-1))
-  else 
+  else
     block(1,1) =  U(N-1)
     block(2,2) =  conjg(U(N-1))
   end if
+
   block(1,2) = -V(N-1)
   block(2,1) =  V(N-1)
   block(:,2) =  block(:,2)*U(N)
+
   if (N > 2) then
     xx = abs(U(N-2))
-    if (xx.GT.0) then
+    if (xx.GT.0d0) then
       block(1,:) = conjg(U(N-2))*block(1,:)/xx
     end if
   end if
-    
-  ! compute eigenvalues and eigenvectors
+
+  ! compute eigenvalues of the trailing 2 x 2 block
   t1 = block
   call z_2x2array_eig(.FALSE.,t1,t1,t2,t2)
-    
+
   ! choose Wilkinson shift
-  ! complex abs does not matter here
-  if(abs(block(2,2)-t1(1,1)) < abs(block(2,2)-t1(2,2)))then
+  if (abs(block(2,2)-t1(1,1)) < abs(block(2,2)-t1(2,2))) then
     rho = t1(1,1)
   else
     rho = t1(2,2)
   end if
 
-  ! compute a nonzero shift
-  ! random shift
+  ! project shift onto the unit circle
   xx = abs(rho)
-  if (xx == 0) then
+
+  if (xx == 0d0) then
     call random_number(xx)
     rho = cmplx(cos(xx),sin(xx),kind=8)
-  ! Wilkinson shift
   else
     rho = rho/xx
   end if
 
-  ! initialize
-  w = -rho
+  ! initialize symmetric turnover parameters
+  !
+  ! Older implementations used a single phase W = -rho.
+  ! In the current standard representation,
+  !
+  !     W = gamma*sigma^2.
+  !
+  ! We choose sigma = 1 and gamma = -rho.
+  gamma = -rho
+  sigma = cmplx(1d0,0d0,kind=8)
   c = 1d0
   s = 0d0
 
   ! main chasing loop
-  do ii=0,(N-1)
+  do ii = 0,(N-1)
 
     ! set ut and vt
     ut = NU*U(ii+1)
     vt = V(ii+1)
 
-    ! turnover
-    call z_rot3_symturnover(w,c,s,ut,vt,rho)
+    ! symmetric turnover
+    call z_rot3_symturnover(gamma,sigma,c,s,ut,vt,rho)
+
+    ! update eigenvectors / similarity accumulator
+    !
+    ! The computed similarity core is
+    !
+    !     Q =
+    !       [ c*sigma       -s              ]
+    !       [ s              c*conjg(sigma)]
+    !
+    ! acting on columns ii+1 and ii+2.
+    !
+    ! The final pass ii = N-1 updates the trailing diagonal core and does not
+    ! correspond to a 2-column update inside the N x N eigenvector matrix.
+    if (VEC .AND. ii < N-1) then
+    
+      do jj = 1,M
+    
+        z1 = Z(jj,ii+1)
+        z2 = Z(jj,ii+2)
+    
+        Z(jj,ii+1) = z1*(c*sigma) + z2*s
+        Z(jj,ii+2) = -z1*s + z2*(c*conjg(sigma))
+    
+      end do
+    
+    else if (VEC) then
+    
+      ! Final diagonal similarity factor Q_N.
+      !
+      ! At ii = N-1, the turnover returns the final one-dimensional
+      ! core transformation.  It acts on the last column of Z by sigma.
+      do jj = 1,M
+        Z(jj,N) = Z(jj,N)*sigma
+      end do
+    
+    end if
 
     ! store ut and vt
-    if ( ii > 0 ) then
+    if (ii > 0) then
       U(ii) = conjg(NU)*ut
       V(ii) = vt
     end if
-    
+
   end do
+
+  ! increment iteration counter
+  ITCNT = ITCNT + 1
 
 end subroutine z_usymfact_singlestep
---
+++ test/complex_double/test_z_usymfact_singlestep.f90
@@ -0,0 +1,390 @@
+#include "eiscor.h"
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!
+! test_z_usymfact_singlestep
+!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!
+! This routine tests one Francis single-shift step for a unitary upper
+! Hessenberg matrix stored in symmetric factored form.
+!
+! Tested subroutine interface:
+!
+!     call z_usymfact_singlestep(VEC,N,U,V,NU,M,Z,ITCNT)
+!
+! Test case:
+!
+!     N  = 256,
+!     ui = 0, vi = 1,  i = 1,...,N-1,
+!     uN = 1, vN = 0,
+!     nu = 1.
+!
+! With VEC = .TRUE., M = N, and Z initialized to the identity, the routine
+! accumulates the unitary similarity transformation used in the QR step.
+! Therefore the output matrix should satisfy
+!
+!     A1 = Z^* A0 Z.
+!
+! The characteristic-polynomial check has been dropped.
+!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+program test_z_usymfact_singlestep
+
+  implicit none
+
+  ! parameters
+  integer, parameter :: N = 256
+  integer, parameter :: M = N
+  real(8), parameter :: eps = (EISCOR_DBL_EPS)
+  real(8), parameter :: tol = 10000d0*(EISCOR_DBL_EPS)
+
+  ! factor variables
+  complex(8) :: U(N), U0(N)
+  real(8) :: V(N), V0(N)
+  complex(8) :: NU
+  integer :: ITCNT
+
+  ! eigenvector/similarity accumulator
+  logical :: VEC
+  complex(8) :: Z(M,N)
+
+  ! dense matrices
+  complex(8) :: A0(N,N), A1(N,N)
+
+  ! timing variables
+  integer :: c_start, c_stop, c_rate
+
+  ! loop index
+  integer :: ii
+
+  ! start timer
+  call system_clock(count_rate=c_rate)
+  call system_clock(count=c_start)
+
+  ! print banner
+  call u_test_banner(__FILE__)
+
+
+
+
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  ! check 1)
+  !
+  ! N = 256,
+  !
+  !     ui = 0, vi = 1,  i = 1,...,N-1,
+  !     uN = 1, vN = 0,
+  !     nu = 1.
+  !
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+  ! initialize factors
+  NU = cmplx(1d0,0d0,kind=8)
+
+  do ii = 1,N-1
+    U(ii) = cmplx(0d0,0d0,kind=8)
+    V(ii) = 1d0
+  end do
+
+  U(N) = cmplx(1d0,0d0,kind=8)
+  V(N) = 0d0
+
+  ITCNT = 0
+
+  ! save input factors
+  U0 = U
+  V0 = V
+
+  ! explicitly form the input matrix
+  call form_usymfact_matrix(N,U0,V0,NU,A0)
+
+  ! initialize eigenvector/similarity accumulator
+  VEC = .TRUE.
+  call set_identity(M,N,Z)
+
+  ! check input normalization
+  call check_factor_normalization(N,U,V,NU,tol,__LINE__)
+
+  ! check input unitarity
+  call check_unitary_matrix(N,A0,tol,__LINE__)
+
+  ! perform one QR step and accumulate the similarity transformation in Z
+  call z_usymfact_singlestep(VEC,N,U,V,NU,M,Z,ITCNT)
+
+  ! explicitly form the output matrix
+  call form_usymfact_matrix(N,U,V,NU,A1)
+
+  ! check output normalization
+  call check_factor_normalization(N,U,V,NU,tol,__LINE__)
+
+  ! check output unitarity
+  call check_unitary_matrix(N,A1,tol,__LINE__)
+
+  ! check that Z is unitary
+  call check_unitary_matrix(N,Z,tol,__LINE__)
+
+  ! check the explicit similarity relation
+  !
+  !     A1 = Z^* A0 Z.
+  !
+  call check_similarity_matrix(N,A0,A1,Z,tol,__LINE__)
+
+
+
+
+  ! stop timer
+  call system_clock(count=c_stop)
+
+  ! print success
+  call u_test_passed(dble(c_stop-c_start)/dble(c_rate))
+
+contains
+
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  !
+  ! set_identity
+  !
+  ! Sets Z = I_N inside an M x N array.
+  !
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  subroutine set_identity(M,N,Z)
+
+    implicit none
+
+    integer, intent(in) :: M, N
+    complex(8), intent(out) :: Z(M,N)
+
+    complex(8) :: zero, one
+    integer :: ii
+
+    zero = cmplx(0d0,0d0,kind=8)
+    one  = cmplx(1d0,0d0,kind=8)
+
+    Z = zero
+
+    do ii = 1,N
+      Z(ii,ii) = one
+    end do
+
+  end subroutine set_identity
+
+
+
+
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  !
+  ! form_usymfact_matrix
+  !
+  ! Forms the dense matrix represented by the symmetric unitary factorization
+  !
+  !     A = diag(NU,1,...,1) * G1 * G2 * ... * G_{N-1}
+  !         * diag(1,...,1,U(N)),
+  !
+  ! where
+  !
+  !     G_i(i:i+1,i:i+1) =
+  !
+  !       [ U(i)       -V(i)        ]
+  !       [ V(i)   conjg(U(i))      ].
+  !
+  ! This implementation applies each core transformation by updating the
+  ! affected columns of A. It avoids constructing a dense G and calling
+  ! MATMUL inside the loop.
+  !
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  subroutine form_usymfact_matrix(N,U,V,NU,A)
+
+    implicit none
+
+    integer, intent(in) :: N
+    complex(8), intent(in) :: U(N)
+    real(8), intent(in) :: V(N)
+    complex(8), intent(in) :: NU
+    complex(8), intent(out) :: A(N,N)
+
+    complex(8) :: zero, one
+    complex(8) :: a1, a2
+    integer :: ii, jj, kk
+
+    zero = cmplx(0d0,0d0,kind=8)
+    one  = cmplx(1d0,0d0,kind=8)
+
+    ! A = identity
+    A = zero
+    do ii = 1,N
+      A(ii,ii) = one
+    end do
+
+    ! left phase diag(NU,1,...,1)
+    A(1,1) = NU
+
+    ! multiply on the right by G_1, ..., G_{N-1}
+    !
+    ! Columns kk and kk+1 are updated by
+    !
+    !     [ col_kk  col_kk+1 ] <- [ col_kk  col_kk+1 ]
+    !
+    !       [ U(kk)       -V(kk)       ]
+    !       [ V(kk)    conjg(U(kk))   ].
+    !
+    do kk = 1,N-1
+
+      do ii = 1,N
+
+        a1 = A(ii,kk)
+        a2 = A(ii,kk+1)
+
+        A(ii,kk)   = a1*U(kk) + a2*V(kk)
+        A(ii,kk+1) = -a1*V(kk) + a2*conjg(U(kk))
+
+      end do
+
+    end do
+
+    ! right phase diag(1,...,1,U(N))
+    do ii = 1,N
+      A(ii,N) = A(ii,N)*U(N)
+    end do
+
+  end subroutine form_usymfact_matrix
+
+
+
+
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  !
+  ! check_factor_normalization
+  !
+  ! Checks
+  !
+  !     |U(i)|^2 + V(i)^2 = 1,   i = 1,...,N-1,
+  !     V(N) = 0,
+  !     |U(N)| = 1,
+  !     |NU| = 1.
+  !
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  subroutine check_factor_normalization(N,U,V,NU,tol,line)
+
+    implicit none
+
+    integer, intent(in) :: N
+    complex(8), intent(in) :: U(N)
+    real(8), intent(in) :: V(N)
+    complex(8), intent(in) :: NU
+    real(8), intent(in) :: tol
+    integer, intent(in) :: line
+
+    real(8) :: err
+    integer :: ii
+
+    err = abs(dble(NU)**2 + aimag(NU)**2 - 1d0)
+
+    do ii = 1,N-1
+      err = max(err,abs(dble(U(ii))**2 + aimag(U(ii))**2 + V(ii)**2 - 1d0))
+    end do
+
+    err = max(err,abs(V(N)))
+    err = max(err,abs(dble(U(N))**2 + aimag(U(N))**2 - 1d0))
+
+    if (err > tol) then
+       call u_test_failed(line)
+    end if
+
+  end subroutine check_factor_normalization
+
+
+
+
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  !
+  ! check_unitary_matrix
+  !
+  ! Checks
+  !
+  !     A^* A = I
+  !
+  ! entrywise.
+  !
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  subroutine check_unitary_matrix(N,A,tol,line)
+
+    implicit none
+
+    integer, intent(in) :: N
+    complex(8), intent(in) :: A(N,N)
+    real(8), intent(in) :: tol
+    integer, intent(in) :: line
+
+    complex(8) :: B(N,N)
+    complex(8) :: zero, one
+    real(8) :: err
+    integer :: ii, jj
+
+    zero = cmplx(0d0,0d0,kind=8)
+    one  = cmplx(1d0,0d0,kind=8)
+
+    B = matmul(conjg(transpose(A)),A)
+
+    err = 0d0
+    do jj = 1,N
+      do ii = 1,N
+        if (ii == jj) then
+          err = max(err,abs(B(ii,jj)-one))
+        else
+          err = max(err,abs(B(ii,jj)-zero))
+        end if
+      end do
+    end do
+
+    if (err > tol) then
+       call u_test_failed(line)
+    end if
+
+  end subroutine check_unitary_matrix
+
+
+
+
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  !
+  ! check_similarity_matrix
+  !
+  ! Checks
+  !
+  !     A1 = Z^* A0 Z.
+  !
+  ! This is the dense form of the accumulated QR-step similarity.
+  !
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  subroutine check_similarity_matrix(N,A0,A1,Z,tol,line)
+
+    implicit none
+
+    integer, intent(in) :: N
+    complex(8), intent(in) :: A0(N,N)
+    complex(8), intent(in) :: A1(N,N)
+    complex(8), intent(in) :: Z(N,N)
+    real(8), intent(in) :: tol
+    integer, intent(in) :: line
+
+    complex(8) :: AZ(N,N), S(N,N)
+    real(8) :: err
+    integer :: ii, jj
+
+    AZ = matmul(A0,Z)
+    S  = matmul(conjg(transpose(Z)),AZ)
+
+    err = 0d0
+    do jj = 1,N
+      do ii = 1,N
+        err = max(err,abs(A1(ii,jj)-S(ii,jj)))
+      end do
+    end do
+
+    if (err > tol) then
+       call u_test_failed(line)
+    end if
+
+  end subroutine check_similarity_matrix
+
+end program test_z_usymfact_singlestep
